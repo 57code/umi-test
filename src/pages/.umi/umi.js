3f916ef7e7e0fc6c11bb669ada6802f0a4cb6c63
yt0379 <yt0379@qq.com>
2019-08-05 17:13:02 +0800

    ff

--- src/pages/.umi/umi.js
+++ src/pages/.umi/umi.js
@@ -34,7 +34,7 @@ Promise.all(moduleBeforeRendererPromises).then(() => {
   window.console && window.console.error(err);
 });
 
-require('../../global.less');
+
 
 // hot module replacement
 if (module.hot) {
